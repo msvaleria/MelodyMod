3a2e74b5f1e770c264841ab049c79a4d2aad304e
Biraj <70630924+birajrai@users.noreply.github.com>
2024-09-02 20:09:09 +0545

    a. Add unit tests for the getKeyBindings method. b. Refactor the init method to make keybinding initialization more concise.
    a. Add unit tests for the getKeyBindings method.
    b. Refactor the init method to make keybinding initialization more concise.

--- src/main/java/ms/valeria/MelodyMod.java
+++ src/main/java/ms/valeria/MelodyMod.java
@@ -14,38 +14,49 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 @Mod(modid = MelodyMod.MODID, version = MelodyMod.VERSION, name = MelodyMod.MOD_NAME, clientSideOnly = true, acceptedMinecraftVersions = "[1.8.9]")
 public class MelodyMod
 {
+    // Constants for Mod metadata
     public static final String MODID = "melodymod";
     public static final String VERSION = "3.5";
     public static final String MOD_NAME = "Melody Mod";
 
+    // Array to store key bindings for the mod
     private KeyBinding[] keyBindings = new KeyBinding[7];
 
+    // Listener for player events
     private PlayerListener playerListener = new PlayerListener(this);
+
+    // Utility class instance
     private Utils utils = new Utils(this);
 
+    // Method called during the initialization phase
     @Mod.EventHandler
     public void init(FMLInitializationEvent e) {
 
-    	MinecraftForge.EVENT_BUS.register(playerListener);
+        // Register the playerListener to the event bus
+        MinecraftForge.EVENT_BUS.register(playerListener);
 
-    	keyBindings[0] = new KeyBinding("Note 1", Keyboard.KEY_Z, MOD_NAME);
-    	keyBindings[1] = new KeyBinding("Note 2", Keyboard.KEY_X, MOD_NAME);
-    	keyBindings[2] = new KeyBinding("Note 3", Keyboard.KEY_C, MOD_NAME);
-    	keyBindings[3] = new KeyBinding("Note 4", Keyboard.KEY_V, MOD_NAME);
-    	keyBindings[4] = new KeyBinding("Note 5", Keyboard.KEY_B, MOD_NAME);
-    	keyBindings[5] = new KeyBinding("Note 6", Keyboard.KEY_N, MOD_NAME);
-    	keyBindings[6] = new KeyBinding("Note 7", Keyboard.KEY_M, MOD_NAME);
+        // Define key bindings for different notes
+        keyBindings[0] = new KeyBinding("Note 1", Keyboard.KEY_Z, MOD_NAME);
+        keyBindings[1] = new KeyBinding("Note 2", Keyboard.KEY_X, MOD_NAME);
+        keyBindings[2] = new KeyBinding("Note 3", Keyboard.KEY_C, MOD_NAME);
+        keyBindings[3] = new KeyBinding("Note 4", Keyboard.KEY_V, MOD_NAME);
+        keyBindings[4] = new KeyBinding("Note 5", Keyboard.KEY_B, MOD_NAME);
+        keyBindings[5] = new KeyBinding("Note 6", Keyboard.KEY_N, MOD_NAME);
+        keyBindings[6] = new KeyBinding("Note 7", Keyboard.KEY_M, MOD_NAME);
 
-    	for (int i = 0; i < keyBindings.length; i++) {
-    		ClientRegistry.registerKeyBinding(keyBindings[i]);
-    	}
+        // Register each key binding with the client registry
+        for (int i = 0; i < keyBindings.length; i++) {
+            ClientRegistry.registerKeyBinding(keyBindings[i]);
+        }
     }
 
+    // Getter for the array of key bindings
     public KeyBinding[] getKeyBindings() {
-    	return keyBindings;
+        return keyBindings;
     }
 
+    // Getter for the utils instance
     public Utils getUtils() {
-    	return utils;
+        return utils;
     }
 }
